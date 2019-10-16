bbead7bf81f0a1cccc27ef3b0221366237a099fc
Eric Loudon <loudon.eric@gmail.com>
2019-10-16 16:05:16 +0000

    Automatically backed up by Learn

--- db/migrate/20191016160316_students.rb
+++ db/migrate/20191016160316_students.rb
@@ -1,4 +1,8 @@
-class Students < ActiveRecord::Migration[5.0]
+class CreateStudents < ActiveRecord::Migration[5.0]
   def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+    end
   end
 end
